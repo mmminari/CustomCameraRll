dc63a35244fddd647288fb740cd5ac963f0ca5f7
mmminari <mmminari@smtown.com>
2017-01-20 17:34:57 +0900

    first commit

---
+++ CustomCameraRoll/CustomCameraRoll/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CustomCameraRoll
+//
+//  Created by 김민아 on 2017. 1. 20..
+//  Copyright © 2017년 김민아. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
